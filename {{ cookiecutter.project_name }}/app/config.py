6f9fcc746ef64b0a8391c8412514a8f42d1faf4d
Vladimir Levin <vladimirlevinozinki@gmail.com>
2024-04-07 16:28:09 +0900

    Cleanup

--- {{ cookiecutter.project_name }}/app/config.py
+++ {{ cookiecutter.project_name }}/app/config.py
@@ -5,7 +5,7 @@ class Config:
     prod: bool
 
     def __init__(self):
-        self.prod = os.environ.get('PRODUCTION', 'false')
+        self.prod = os.environ.get('PRODUCTION', 'false') == 'true'
 
 
 def get_config() -> Config:
